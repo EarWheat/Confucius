bc5c019198306e4f4fbe0736260e32265e0132a4
liuzhaoluliuzhaolu <liuzhaoluliuzhaolu@didiglobal.com>
2020-09-22 16:15:33 +0800

    项目初始化

---
+++ src/main/java/com/education/confucius/ConfuciusApplication.java
@@ -0,0 +1,13 @@
+package com.education.confucius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfuciusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfuciusApplication.class, args);
+	}
+
+}
