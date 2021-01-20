d8f84850a44c95af59df81265b38a60f35e521e8
EarWheat <2636965138@qq.com>
2021-01-20 19:50:36 +0800

    tomcat启动

--- src/main/java/com/education/confucius/ConfuciusApplication.java
+++ src/main/java/com/education/confucius/ConfuciusApplication.java
@@ -2,15 +2,23 @@ package com.education.confucius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(value = "com.pangu")
 @ComponentScan(value = "com.education")
-public class ConfuciusApplication {
+public class ConfuciusApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(ConfuciusApplication.class);
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfuciusApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(ConfuciusApplication.class);
+		springApplication.run(args);
 	}
 
 }
