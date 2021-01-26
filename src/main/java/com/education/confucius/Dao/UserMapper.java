360e958608c0946190e6100a1e91240521266585
EarWheat <2636965138@qq.com>
2021-01-26 19:47:57 +0800

    用户类Mapper

---
+++ src/main/java/com/education/confucius/Dao/UserMapper.java
@@ -0,0 +1,18 @@
+package com.education.confucius.Dao;
+
+import com.education.confucius.Entity.User.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author liuzhaoluliuzhaolu
+ * @date 2021/1/26 下午7:39
+ * @desc 用户查询类
+ * @prd
+ * @Modification History:
+ * Date         Author          Description
+ * ------------------------------------------ *
+ */
+@Mapper
+public interface UserMapper {
+    User getUserByName(String name);
+}
